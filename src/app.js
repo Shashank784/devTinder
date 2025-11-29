89cf3e20127f0502ce8f0461c1e5a61e0b2ead8d
Shashank784 <Shashank.Sagar@dataart.com>
2025-11-29 17:48:34 +0530

    Adding params, query

--- src/app.js
+++ src/app.js
@@ -2,27 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use("/user", (req, res)=> {
-    res.send('Hello User!');
-})
+// app.get("/user", (req, res)=> {
+//     console.log(req.query)
+//     res.send({firstName: "shashank", LastName: "sagar"});
+// });
 
-app.get("/user", (req, res)=> {
-    res.send({firstName:'shashank', lastName:'sagar'});
+
+app.get("/user/:userId/:name/:password", (req, res)=> {
+    console.log(req.params);
+    res.send({firstName: "shashank", LastName: "sagar"});
 });
 
-app.post("/user", (req, res)=> {
-    //saving data to database
-    res.send("Data Received");
-});
-
-app.delete("/user", (req, res)=> {
-    //saving data to database
-    res.send("Delated Successfully");
-});
-
-app.use("/test", (req, res)=> {
-    res.send('Hello from server!');
-});
+ 
 
 app.listen(4000, ()=> {
     console.log('Server is running on port 4000 ...');
