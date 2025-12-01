13fbcec8ba14c1e2abbc9a0cc2c79a69ad794fa1
Shashank784 <Shashank.Sagar@dataart.com>
2025-12-01 13:57:45 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -8,13 +8,28 @@ const app = express();
 // });
 
 
-app.get("/user/:userId/:name/:password", (req, res)=> {
-    console.log(req.params);
-    res.send({firstName: "shashank", LastName: "sagar"});
+// app.get("/user/:userId/:name/:password", (req, res)=> {
+//     console.log(req.params);
+//     res.send({firstName: "shashank", LastName: "sagar"});
+// });
+
+//GET / Users => middleware chain => request handlet
+
+app.use("/", (req, res, next)=> {
+    // res.send("handling / route");
+    next();
 });
 
+app.get("/user", (req, res, next)=> {
+    console.log("Handleing /user routes");
+    next();
+},
+(req, res, next)=> {
+    res.send("1st Route Handler");
+},(req, res, next)=> {
+    res.send("2nd Route Handler");
+});
  
-
 app.listen(4000, ()=> {
     console.log('Server is running on port 4000 ...');
 });
